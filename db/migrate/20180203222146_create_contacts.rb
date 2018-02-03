2da24d3cdae44b01366bc134896be3e5be8e9799
Simon Jordan <simon_jordan@live.com>
2018-02-03 23:50:55 +0000

    updated:

--- db/migrate/20180203222146_create_contacts.rb
+++ db/migrate/20180203222146_create_contacts.rb
@@ -1,6 +1,7 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
+      
       t.string :name
       t.string :email
       t.text :comments
--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20180203222146) do
 
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
